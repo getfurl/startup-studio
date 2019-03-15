0eb3d7b706730af86453269b6442984b76a33f8e
Liran Sharir <lsharir@gmail.com>
2019-03-15 14:42:00 -0400

    link ripple

--- client/src/app/shared/material/material.module.ts
+++ client/src/app/shared/material/material.module.ts
@@ -11,7 +11,8 @@ import {
   MatProgressBarModule,
   MatBadgeModule,
   MatDividerModule,
-  MatSnackBarModule
+  MatSnackBarModule,
+  MatRippleModule
 } from "@angular/material";
 
 const MODULES = [
@@ -26,7 +27,8 @@ const MODULES = [
   MatProgressBarModule,
   MatBadgeModule,
   MatDividerModule,
-  MatSnackBarModule
+  MatSnackBarModule,
+  MatRippleModule
 ];
 
 @NgModule({
